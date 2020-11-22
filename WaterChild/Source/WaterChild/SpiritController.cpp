5165b06c9da1c7dc399b9fb11d3ce132cc9286d1
Chan Zhe <chanzhe99@gmail.com>
2020-11-23 05:31:24 +0800

    - UI Polishing finished

--- WaterChild/Source/WaterChild/SpiritController.cpp
+++ WaterChild/Source/WaterChild/SpiritController.cpp
@@ -13,18 +13,18 @@ void ASpiritController::SetupInputComponent()
 	InputComponent->BindAction("ClimbButton", IE_Pressed, this, &ASpiritController::ClimbAction);
 	InputComponent->BindAction("ClimbButton", IE_Released, this, &ASpiritController::StopClimbAction);
 	InputComponent->BindAction("JumpButton", IE_Pressed, this, &ASpiritController::JumpAction);
-	InputComponent->BindAction("PauseButton", IE_Pressed, this, &ASpiritController::PauseAction);
-	InputComponent->BindAction("AnyKey", IE_Pressed, this, &ASpiritController::AnyKeyAction);
+	InputComponent->BindAction("PauseButton", IE_Pressed, this, &ASpiritController::PauseAction).bExecuteWhenPaused = true;
+	InputComponent->BindAction("AnyKey", IE_Pressed, this, &ASpiritController::AnyKeyAction).bExecuteWhenPaused = true;
 
 	// Binds the movement axes
 	InputComponent->BindAxis("KeyboardForwardAxis", this, &ASpiritController::KeyboardForwardAxis);
 	InputComponent->BindAxis("KeyboardRightAxis", this, &ASpiritController::KeyboardRightAxis);
-	InputComponent->BindAxis("GamepadForwardAxis", this, &ASpiritController::GamepadForwardAxis);
-	InputComponent->BindAxis("GamepadRightAxis", this, &ASpiritController::GamepadRightAxis);
+	InputComponent->BindAxis("GamepadForwardAxis", this, &ASpiritController::GamepadForwardAxis).bExecuteWhenPaused = true;
+	InputComponent->BindAxis("GamepadRightAxis", this, &ASpiritController::GamepadRightAxis).bExecuteWhenPaused = true;
 
 	// Binds the look axes
 	InputComponent->BindAxis("MouseLookUpAxis", this, &ASpiritController::MouseLookUpAxis);
 	InputComponent->BindAxis("MouseTurnAxis", this, &ASpiritController::MouseTurnAxis);
-	InputComponent->BindAxis("GamepadLookUpAxis", this, &ASpiritController::GamepadLookUpAxis);
-	InputComponent->BindAxis("GamepadTurnAxis", this, &ASpiritController::GamepadTurnAxis);
+	InputComponent->BindAxis("GamepadLookUpAxis", this, &ASpiritController::GamepadLookUpAxis).bExecuteWhenPaused = true;
+	InputComponent->BindAxis("GamepadTurnAxis", this, &ASpiritController::GamepadTurnAxis).bExecuteWhenPaused = true;
 }
